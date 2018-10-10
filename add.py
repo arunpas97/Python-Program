6ddb264fb5b36bf55fc04f84b677b9d5e4487ead
Arunkumar Palani <arunkumarpas002@gmail.com>
2018-10-10 12:09:43 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,9 @@
+class ab:
+        def __init__(self):
+                a=int(input("Enter the value :"))
+                b=int(input("Enter the value :"))
+                c=a+b
+                print("Additon :",c)
+
+obj=ab()
+
